587ae67240f7a2ff7b5d5c5e7d3099c45a9bc7c3
Christian George <lr.cgsegundo@mmdc.mcl.edu.ph>
2025-02-25 18:56:37 +0800

    initial

---
+++ Documents/NetBeansProjects/mavenproject1/CP1/CP1/src/main/java/com/mycompany/cp1/MainClass.java
@@ -0,0 +1,17 @@
+
+
+
+package com.mycompany.cp1;
+
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+         //   private  String currentPath = System.getProperty("user.dir");
+            
+     System.out.println("SFSDGSDGSDGS");
+     //   EmployeeInfo empinf  = new EmployeeInfo();
+      //  empinf.fetchEmployeeInfo();
+    }
+}
