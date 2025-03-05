087b9263c6ba01a38e988209dac01727279f6999
Christian George <lr.cgsegundo@mmdc.mcl.edu.ph>
2025-03-05 20:50:20 +0800

    GrossSalaryPerweek_new encapsulated

--- Documents/NetBeansProjects/mavenproject1/CP1/CP1/src/main/java/com/mycompany/cp1/MainClass.java
+++ Documents/NetBeansProjects/mavenproject1/CP1/CP1/src/main/java/com/mycompany/cp1/MainClass.java
@@ -3,15 +3,25 @@
 
 package com.mycompany.cp1;
 
+import java.io.IOException;
+
 
 public class MainClass {
 
     public static void main(String[] args) {
 
-         //   private  String currentPath = System.getProperty("user.dir");
-            
-     System.out.println("SFSDGSDGSDGS");
-     //   EmployeeInfo empinf  = new EmployeeInfo();
-      //  empinf.fetchEmployeeInfo();
+             int weekNumber = 29;  // Example week number
+        String employeeNumber = "10003";  // Example employee number
+
+        GrossSalaryPerWeek_NEW calculator = new GrossSalaryPerWeek_NEW();
+        try {
+            calculator.calculateGrossSalary(weekNumber, employeeNumber);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    
+    
+
+           
 }
